d0200d716bce6fb8677291bb8d69b8d3629b9530
Dmitry <comradmajor@gmail.com>
2026-02-12 00:14:37 +0400

    Edit comment section on est_model.py, delete all unused schemas on pydantic_schemas.py

--- est_model.py
+++ est_model.py
@@ -2,11 +2,11 @@ import numpy as np
 
 class Estimator:
     def __init__(self):
-        # Веса для фичей: avg_check, last_category_id, loyalty_score
+        # Scores for features avg_check, last_category_id, loyalty_score
         self.weights = np.array([0.475, 0.05, 0.475])
 
     def predict(self, features: list):
-        # Быстрое матричное умножение (Dot product)
+        # Fast matrix multiplication (Dot product)
         score = np.dot(features, self.weights)
         return float(score)
 
